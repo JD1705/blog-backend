e593357bd9acb4f9bb793af39f1974164530c38f
jd1705 <soulbusterjd17@gmail.com>
2025-11-18 18:27:03 -0400

    fix: change settings default values

--- src/core/config.py
+++ src/core/config.py
@@ -1,13 +1,16 @@
 from pydantic_settings import BaseSettings
 from pydantic import Field
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Settings(BaseSettings):
     # MongoDB configuration
-    mongodb_url: str = Field("MONGODB_URL",alias="MONGODB_URL")
+    mongodb_url: str = Field(alias="MONGODB_URL")
     database_name: str = Field("blog_db", alias="DATABASE_NAME")
 
     # JWT configuration
-    jwt_secret_key: str = Field("JWT_SECRET_KEY",alias="JWT_SECRET_KEY")
+    jwt_secret_key: str = Field(alias="JWT_SECRET_KEY")
     jwt_algorithm: str = Field("HS256", alias="JWT_ALGORITHM")
     jwt_expire_minutes: int = Field(30, alias="JWT_EXPIRE_MINUTES")
     
