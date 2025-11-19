4ab0e4358faf17c93bf3fad0d8cd17dea0990e58
jd1705 <soulbusterjd17@gmail.com>
2025-11-19 17:51:32 -0400

    chore: add auth router to main

--- src/main.py
+++ src/main.py
@@ -1,9 +1,13 @@
 from core.config import settings
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 from core.database import db
 from services.utility import lifespan
+from api.routes import auth
 
 app = FastAPI(title="Blog API", lifespan=lifespan)
+
+# routers
+app.include_router(auth.router)
 
 @app.get("/")
 async def root():
