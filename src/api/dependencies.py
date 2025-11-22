0b2c08356a076a9e90ffae797373565afb9a8e5e
jd1705 <soulbusterjd17@gmail.com>
2025-11-22 14:30:28 -0400

    chore: create get token and current user dependencies

---
+++ src/api/dependencies.py
@@ -0,0 +1,48 @@
+from fastapi import Depends, HTTPException, status
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from core.security import verify_token
+from core.database import db
+from bson import ObjectId
+
+security = HTTPBearer()
+
+async def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(security)):
+
+    try:
+        token = credentials.credentials
+        payload = verify_token(token)
+
+        user_id = payload.get("sub")
+        if not user_id:
+            raise HTTPException(
+                    status_code=status.HTTP_401_UNAUTHORIZED,
+                    detail="Invalid Token: it does not have user_id"
+                    )
+        
+        collection = db.database.get_collection("users") # type: ignore
+        user = collection.find_one({"_id":ObjectId(user_id)})
+        if not user:
+            raise HTTPException(
+                    status_code=status.HTTP_404_NOT_FOUND,
+                    detail="User Not Found"
+                    )
+
+        return user
+
+    except Exception as e:
+        raise e
+
+async def get_token(credentials: HTTPAuthorizationCredentials = Depends(security)):
+    token = credentials.credentials
+    try:
+        payload = verify_token(token)
+        user_id = payload.get("sub")
+        if not user_id:
+            raise HTTPException(
+                    detail="Invalid Token",
+                    status_code=status.HTTP_401_UNAUTHORIZED
+                    )
+
+        return token
+    except Exception as e:
+        raise e
