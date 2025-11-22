74738d55f2a55ba79ae17bdadc5cd5b7f319b425
jd1705 <soulbusterjd17@gmail.com>
2025-11-22 14:31:35 -0400

    chore: add logout endpoint

--- src/api/routes/auth.py
+++ src/api/routes/auth.py
@@ -1,6 +1,7 @@
 from services.auth_services import AuthService
 from schemas.user import UserCreate, UserResponse, UserLogin
-from fastapi import APIRouter, status, Depends
+from fastapi import APIRouter, status, Depends, HTTPException
+from api.dependencies import get_token
 
 router = APIRouter(prefix="/auth")
 
@@ -24,3 +25,22 @@ async def login(user_data: UserLogin, auth_service: AuthService = Depends()):
     response = await auth_service.login_user(user_data)
 
     return response
+
+@router.post("/logout")
+async def logout(token: str = Depends(get_token), auth_service: AuthService = Depends()):
+    token_in_blacklist = await auth_service.is_blacklisted_token(token)
+    if token_in_blacklist == False:
+        success = await auth_service.logout_user(token)
+
+        if success:
+            return {"message":"Logout Successful"}
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="logout failed"
+                    )
+    else:
+        raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="You are not authenticated"
+                )
