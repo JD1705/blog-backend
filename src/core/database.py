884a24afbbb403f46d9e67f3877a8b73556327af
jd1705 <soulbusterjd17@gmail.com>
2025-11-23 11:52:43 -0400

    chore: create indexes setup

--- src/core/database.py
+++ src/core/database.py
@@ -1,4 +1,4 @@
-from pymongo import AsyncMongoClient
+from pymongo import AsyncMongoClient, ASCENDING
 from core.config import settings
 
 class Database:
@@ -17,5 +17,25 @@ class Database:
         if self.client:
             await self.client.close()
         print("\033[32mINFO\033[0m:     Connection Closed")
+
+    async def setup_database_indexes(self):
+        """Configura todos los índices necesarios al iniciar la app"""
+        database = self.database.get_collection("token_blacklist") # type: ignore 
+    
+        # TTL Index para token_blacklist
+        await database.token_blacklist.create_index(
+            [("expires_at", ASCENDING)],
+            expireAfterSeconds=0,
+            name="ttl_expires_at"
+        )
+        
+        # Índices únicos para users
+        await database.users.create_index(
+            [("email", ASCENDING)], 
+            unique=True,
+            name="unique_email"
+        )
+
+        print("\033[32mINFO\033[0m:     Indexes Created")
 
 db = Database()
