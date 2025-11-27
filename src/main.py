0496f343de3e42cd0379d43dfd2128ec443ba371
jd1705 <soulbusterjd17@gmail.com>
2025-11-27 11:22:24 -0400

    feat: add users router

--- src/main.py
+++ src/main.py
@@ -2,12 +2,13 @@ from core.config import settings
 from fastapi import FastAPI, status
 from core.database import db
 from services.utility import lifespan
-from api.routes import auth
+from api.routes import auth, users
 
 app = FastAPI(title="Blog API", lifespan=lifespan)
 
 # routers
 app.include_router(auth.router)
+app.include_router(users.router)
 
 @app.get("/")
 async def root():
