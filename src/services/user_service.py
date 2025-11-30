d8fbde5afbcb9874308eed1b36aa9c6ce7de6314
jd1705 <soulbusterjd17@gmail.com>
2025-11-30 17:48:33 -0400

    chore: create profile update method

--- src/services/user_service.py
+++ src/services/user_service.py
@@ -6,19 +6,72 @@ from fastapi import HTTPException, status
 from datetime import datetime, timezone, timedelta
 from models.token import TokenBlacklist
 
+
 class UserService:
     def __init__(self):
         self.database = db.database
 
     async def get_user_profile(self, current_user: dict):
         user_data = UserResponse(
-                id=str(current_user["_id"]),
-                username=current_user["username"],
-                bio=current_user["bio"],
-                email=current_user["email"],
-                role=current_user["role"],
-                post_count=current_user["posts_count"],
-                created_at=current_user["created_at"]
-                )
+            id=str(current_user["_id"]),
+            username=current_user["username"],
+            bio=current_user["bio"],
+            email=current_user["email"],
+            role=current_user["role"],
+            post_count=current_user["posts_count"],
+            created_at=current_user["created_at"],
+        )
 
         return user_data
+
+    async def update_user_profile(self, current_user: dict, update_data: UserUpdate):
+        collection = self.database.get_collection("users")  # type: ignore
+        data = update_data.model_dump()
+        to_update = {}
+        fields = ["email", "username", "bio"]
+
+        if data["email"] is not None:
+            normalized_email = update_data.email.lower().strip()  # type: ignore
+            user_exists = await collection.find_one({"email": normalized_email})
+
+            if not user_exists:
+                for field in fields:
+                    if data[field] is not None:
+                        to_update.update({field: data[field]})
+
+                to_update.update({"updated_at": datetime.now(timezone.utc)})
+                await collection.update_one(
+                    {"_id": current_user["_id"]}, {"$set": to_update}
+                )
+                updated_user = await collection.find_one({"_id": current_user["_id"]})
+
+                return UserResponse(
+                    id=str(updated_user["_id"]),  # type: ignore
+                    username=updated_user["username"],  # type: ignore
+                    bio=updated_user["bio"],  # type: ignore
+                    email=updated_user["email"],  # type: ignore
+                    role=updated_user["role"],  # type: ignore
+                    post_count=updated_user["posts_count"],  # type: ignore
+                    created_at=updated_user["created_at"],  # type: ignore
+                )
+
+        else:
+            for field in fields:
+                if data[field] is not None:
+                    to_update.update({field: data[field]})
+
+            to_update.update({"updated_at": datetime.now(timezone.utc)})
+            await collection.update_one(
+                {"_id": current_user["_id"]}, {"$set": to_update}
+            )
+            updated_user = await collection.find_one({"_id": current_user["_id"]})
+
+            return UserResponse(
+                id=str(updated_user["_id"]),  # type: ignore
+                username=updated_user["username"],  # type: ignore
+                bio=updated_user["bio"],  # type: ignore
+                email=updated_user["email"],  # type: ignore
+                role=updated_user["role"],  # type: ignore
+                post_count=updated_user["posts_count"],  # type: ignore
+                created_at=updated_user["created_at"],  # type: ignore
+            )
