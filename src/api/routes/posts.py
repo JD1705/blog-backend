9a3aee6e6540f6b3ef78eed2c6dd939f3c21aacd
jd1705 <soulbusterjd17@gmail.com>
2025-12-15 11:18:13 -0400

    chore: create router and creation endpoint

---
+++ src/api/routes/posts.py
@@ -0,0 +1,35 @@
+from fastapi import APIRouter, HTTPException, status, Depends
+from api.dependencies import get_current_user
+from models.user import User
+from services.post_service import PostService
+from schemas.post import PostCreate, PostFilters, PostUpdate, PostResponse
+
+router = APIRouter(prefix="/posts")
+
+
+@router.post("/")
+async def create_posts(
+    post_data: PostCreate,
+    post_service: PostService = Depends(),
+    author: dict = Depends(get_current_user),
+):
+    response = await post_service.create_post(
+        post_data, author=User.from_mongo_dict(author)
+    )
+
+    return PostResponse(
+        title=response.title,
+        content=response.content,
+        author_id=str(response.author_id),
+        tags=response.tags,
+        featured_image=response.featured_image,
+        id=str(response.id),
+        slug=response.slug,
+        author_username=response.author_username,
+        status=response.status,
+        view_count=response.view_count,
+        comments_count=response.comments_count,
+        created_at=response.created_at,
+        updated_at=response.updated_at,
+        published_at=response.published_at,
+    )
