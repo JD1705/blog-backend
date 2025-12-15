cb43d9a133cbee148713824fbafcee7706ea4a35
jd1705 <soulbusterjd17@gmail.com>
2025-12-15 11:18:37 -0400

    feat: add post router

--- src/main.py
+++ src/main.py
@@ -2,15 +2,16 @@ from core.config import settings
 from fastapi import FastAPI, status
 from core.database import db
 from services.utility import lifespan
-from api.routes import auth, users
+from api.routes import auth, users, posts
 
 app = FastAPI(title="Blog API", lifespan=lifespan)
 
 # routers
 app.include_router(auth.router)
 app.include_router(users.router)
+app.include_router(posts.router)
+
 
 @app.get("/")
 async def root():
-    return {"message": "Blog API Running",
-            "status": "OK"}
+    return {"message": "Blog API Running", "status": "OK"}
