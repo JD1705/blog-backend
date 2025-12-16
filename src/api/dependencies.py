ed6c62985ef040c744fc97b71c75191eae1b2042
jd1705 <soulbusterjd17@gmail.com>
2025-12-16 19:38:10 -0400

    chore: add None return for optional auth endpoints

--- src/api/dependencies.py
+++ src/api/dependencies.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from fastapi import Depends, HTTPException, status
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from core.security import verify_token
@@ -5,10 +6,15 @@ from core.database import db
 from bson import ObjectId
 from services.auth_services import AuthService
 
-security = HTTPBearer()
+security = HTTPBearer(auto_error=False)
 auth_service = AuthService()
 
-async def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(security)):
+
+async def get_current_user(
+    credentials: Optional[HTTPAuthorizationCredentials] = Depends(security),
+):
+    if credentials is None:
+        return None
 
     try:
         token = credentials.credentials
@@ -17,34 +23,35 @@ async def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(s
         user_id = payload.get("sub")
         if not user_id:
             raise HTTPException(
-                    status_code=status.HTTP_401_UNAUTHORIZED,
-                    detail="Invalid Token: it does not have user_id"
-                    )
-        
-        collection = db.database.get_collection("users") # type: ignore
-        user = await collection.find_one({"_id":ObjectId(user_id)})
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Invalid Token: it does not have user_id",
+            )
+
+        collection = db.database.get_collection("users")  # type: ignore
+        user = await collection.find_one({"_id": ObjectId(user_id)})
         if not user:
             raise HTTPException(
-                    status_code=status.HTTP_404_NOT_FOUND,
-                    detail="User Not Found"
-                    )
-        
+                status_code=status.HTTP_404_NOT_FOUND, detail="User Not Found"
+            )
+
         elif not user["is_active"] == True:
             raise HTTPException(
-                    status_code=status.HTTP_401_UNAUTHORIZED, detail="Account is deactivated"
-                    )
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Account is deactivated",
+            )
 
         is_token_blakclisted = await auth_service.is_blacklisted_token(token)
         if is_token_blakclisted == True:
             raise HTTPException(
-                    status_code=status.HTTP_401_UNAUTHORIZED,
-                    detail="You are not authenticated"
-                    )
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="You are not authenticated",
+            )
 
         return user
 
     except Exception as e:
         raise e
+
 
 async def get_token(credentials: HTTPAuthorizationCredentials = Depends(security)):
     token = credentials.credentials
@@ -53,9 +60,8 @@ async def get_token(credentials: HTTPAuthorizationCredentials = Depends(security
         user_id = payload.get("sub")
         if not user_id:
             raise HTTPException(
-                    detail="Invalid Token",
-                    status_code=status.HTTP_401_UNAUTHORIZED
-                    )
+                detail="Invalid Token", status_code=status.HTTP_401_UNAUTHORIZED
+            )
 
         return token
     except Exception as e:
