ed3ff94c1f83d30479f4f511d09287a23e787c4d
jd1705 <soulbusterjd17@gmail.com>
2025-12-16 19:42:52 -0400

    feat: create endpoint for post search by slug

--- src/api/routes/posts.py
+++ src/api/routes/posts.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from fastapi import APIRouter, HTTPException, status, Depends
 from api.dependencies import get_current_user
 from models.user import User
@@ -16,6 +17,32 @@ async def create_posts(
     response = await post_service.create_post(
         post_data, author=User.from_mongo_dict(author)
     )
+
+    return PostResponse(
+        title=response.title,
+        content=response.content,
+        author_id=str(response.author_id),
+        tags=response.tags,
+        featured_image=response.featured_image,
+        id=str(response.id),
+        slug=response.slug,
+        author_username=response.author_username,
+        status=response.status,
+        view_count=response.view_count,
+        comments_count=response.comments_count,
+        created_at=response.created_at,
+        updated_at=response.updated_at,
+        published_at=response.published_at,
+    )
+
+
+@router.get("/{slug}")
+async def get_post_by_slug(
+    slug: str,
+    post_service: PostService = Depends(),
+    current_user: Optional[dict] = Depends(get_current_user),
+):
+    response = await post_service.get_post_by_slug(slug, current_user)
 
     return PostResponse(
         title=response.title,
