72022fd754f90faf69fbd95e100a632923be6cc0
jd1705 <soulbusterjd17@gmail.com>
2026-01-21 17:59:36 -0400

    feat: create endpoint to edit comments

--- src/api/routes/comments.py
+++ src/api/routes/comments.py
@@ -47,8 +47,40 @@ async def get_comments_in_post(
     current_user: Optional[dict] = Depends(get_current_user),
     sort_by: str = "newest",
     limit: int = 100,
-    offset: int = 0
+    offset: int = 0,
 ):
-    response = await comment_service.get_comments(slug, sort_by, limit, offset, current_user)
+    response = await comment_service.get_comments(
+        slug, sort_by, limit, offset, current_user
+    )
 
     return response
+
+
+@router.put("/{comment_id}")
+async def update_comment(
+    slug: str,
+    comment_id: str,
+    update_data: CommentUpdate,
+    comment_service: CommentService = Depends(),
+    current_user: dict = Depends(get_current_user),
+):
+    response = await comment_service.update_comment(
+        slug, comment_id, update_data, current_user
+    )
+
+    comment_response = CommentResponse(
+        id=str(response.id),
+        author_id=str(response.author_id),
+        content=response.content,
+        author_username=response.author_username,
+        parent_id=str(response.parent_id),
+        created_at=response.created_at,
+        deleted_at=response.deleted_at,
+        is_deleted=response.is_deleted,
+        is_edited=response.is_edited,
+        replies_count=response.replies_count,
+        post_id=str(response.post_id),
+        updated_at=response.updated_at,
+        edited_at=response.edited_at,
+    )
+    return comment_response
